bc80bd1c48d056342694283de77b20a6e183ab4d
Simphiwe Tebe <simphiwetebe@gmail.com>
2021-06-30 17:52:50 +0200

    complete carousel UI and animation

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -2,11 +2,16 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import './hero.scss'
 import {slides} from '../../data'
+import {FaAngleRight, FaAngleLeft} from 'react-icons/fa'
+import gsap from 'gsap'
 
 function Hero() {
 
     const [slideIndex, setSlideIndex] = React.useState({ count: 0 })
     const numSlides = slides.length -1;
+
+    let subtextEL = React.useRef(null);
+    let actionEL = React.useRef(null);
 
     const handleNext = () => {
 
@@ -28,17 +33,30 @@ function Hero() {
 
     }
 
+    React.useEffect(()=> {
+        gsap.from([subtextEL, actionEL], {
+            duration: 1,
+            opacity: 0,
+            height: 20,
+            transformOrigin: 'bottom',
+            ease: 'power3.inOut',
+            stagger: {
+              amount: 0.1
+            }
+        })
+    },[slideIndex])
+
     return (
         <header className="hero">
             <div className="hero-slide" style={{ backgroundImage: `url(${slides[slideIndex.count].img})`, backgroundSize: 'cover', backgroundPosition: 'center'}}>
-                <div className="hero-title">
-                    <h4>{`${slides[slideIndex.count].title}`}</h4>
-                    <p>Some subtext here</p>
-                    <Link to="/shop">Shop now</Link>
+                <div className="hero-slide__title">
+                <h4>{`${slides[slideIndex.count].title}`}</h4>
+                    <p ref={(el) =>{subtextEL = el}}>{`${slides[slideIndex.count].subtext}`}</p>
+                    <Link to="/shop" className="hero-slide__cta" ref={(el) =>{actionEL = el}}>Shop now</Link>
                 </div>
-                <div className="hero-slide-controls">
-                    <button className="prev" onClick={()=> handlePrev()}>B</button>
-                    <button className="next" onClick={()=> handleNext()}>F</button>
+                <div className="hero-slide__controls">
+                    <button className="prev" onClick={()=> handlePrev()}><FaAngleLeft /></button>
+                    <button className="next" onClick={()=> handleNext()}><FaAngleRight /></button>
                 </div>
             </div>
         </header>
--- src/data.js
+++ src/data.js
@@ -1,25 +1,30 @@
-import slide1 from './images/slides/carousel-1.jpg';
+import slide1 from './images/slides/carousel-2.jpeg';
+import slide2 from './images/slides/carousel-3.jpeg';
+import slide3 from './images/slides/carousel-1.jpeg';
 
 export const slides = [
     {
         id: 0,
-        title: 'The Best of Slide One',
-        subtext: 'Some subtext to go in here.',
-        cta: '/shop',
+        title: 'From bottles to Fashion',
+        subtext: 'Find all kiddies collection in here.',
+        cta: 'View Now',
+        link: '/shop',
         img: slide1
     },
     {
         id: 1,
-        title: 'The Best of Slide Two',
-        subtext: 'Some subtext to go in here.',
-        cta: '/shop',
-        img: slide1
+        title: 'Make it Reign',
+        subtext: 'Sneaker collection',
+        cta: 'Shop Now',
+        link: '/shop',
+        img: slide2
     },
     {
         id: 2,
-        title: 'The Best of Slide Three',
-        subtext: 'Some subtext to go in here.',
-        cta: '/shop',
-        img: slide1
+        title: 'Win a trip for Three',
+        subtext: 'Check competition page for more details.',
+        cta: 'View Now',
+        link: '/shop',
+        img: slide3
     }
 ]
